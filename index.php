f7191362c2809a78e9213839f0a476d71a0bc52f
sachin200453@gmail.com <mailnarendra15@gmail.com>
2013-03-13 18:14:24 -0800

    initialization of all files

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Login</title>
+    <link href="css/style.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+		<div class="form-signin">
+      <form>
+        <h2 class="form-signin-heading">Enter your USN</h2>
+        <input type="text" class="input-block-level" placeholder="USN">
+        <button class="btn btn-large" type="submit">Log in</button>
+      </form>
+
+    </div>
+  </body>
+</html>
