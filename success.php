c81bf0bab555d473efbd9ed6e5f517f3e38dbeb9
sachin200453@gmail.com <mailnarendra15@gmail.com>
2013-03-14 09:02:59 -0800

    added variable for each data

--- success.php
+++ success.php
@@ -12,26 +12,23 @@ mysql_select_db("$db_name")or die("cannot select DB");
 $usn=$_SESSION['usn'];
 $topic=$_POST['topic'];
 $presenter=$_POST['presenter'];
-$q1=$_POST['q1'];
-$q2=$_POST['q2'];
-$q3=$_POST['q3'];
-$q4=$_POST['q4'];
-$q5=$_POST['q5'];
-$q6=$_POST['q6'];
-$q7=$_POST['q7'];
-$q8=$_POST['q8'];
-$q9=$_POST['q9'];
-$q10=$_POST['q10'];
-$q11=$_POST['q11'];
-$q12=$_POST['q12'];
+$q1=intval($_POST['q1']);
+$q2=intval($_POST['q2']);
+$q3=intval($_POST['q3']);
+$q4=intval($_POST['q4']);
+$q5=intval($_POST['q5']);
+$q6=intval($_POST['q6']);
+$q7=intval($_POST['q7']);
+$q8=intval($_POST['q8']);
+$q9=intval($_POST['q9']);
+$q10=intval($_POST['q10']);
+$q11=intval($_POST['q11']);
+$q12=intval($_POST['q12']);
 
-$avg = (intval($_POST['q1'])+intval($_POST['q2'])+intval($_POST['q3'])+intval($_POST['q4'])+intval($_POST['q5'])+intval($_POST['q6'])+intval($_POST['q7'])+intval($_POST['q8'])+intval($_POST['q9'])+intval($_POST['q10'])+intval($_POST['q11'])+intval($_POST['q12']))/12;
-print $avg;
+$avg = ($q1+$q2+$q3+$q4+$q5+$q6+$q7+$q8+$q9+$q10+$q11+$q12)/12;
+
 $sql="INSERT INTO $tbl_name VALUES('$usn', '$topic', '$presenter', '$q1', '$q2', '$q3', '$q4', '$q5', '$q6', '$q7', '$q8', '$q9', '$q10', '$q11', '$q12', '$avg')";
 $result=mysql_query($sql);
-
-//$sql="INSERT INTO $tbl_name(topic, detail, name, email, datetime)VALUES('$topic', '$detail', '$name', '$email', '$datetime')";
-//$result=mysql_query($sql);
 
 mysql_close();
 
