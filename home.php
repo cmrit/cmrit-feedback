988c18c1f9c3afc4001e6656edfdccae74583203
sachin200453@gmail.com <mailnarendra15@gmail.com>
2013-03-13 20:14:45 -0800

    structured home page

--- home.php
+++ home.php
@@ -8,7 +8,55 @@
   </head>
 
   <body>
-		<div class="container">
+		<div class="container page">
+		<div id="header">
+		<div class="pull-left">
+		<img src="images/logo-small.png" />
+		</div>
+		<div class="span4 header-title">
+		<h2>Feedback Form</h2>
+		</div>
+		</div>
+		<hr />
+		<div class="row-fluid">
+		<div class="span4">
+		<form>
+
+          <label class="control-label" for="topic"><i class="icon-user"></i> Topic</label>
+          <input type="text" class="input-xlarge" id="topic" placeholder="Topic">
+
+		
+            <label class="control-label" for="presenter"><i class="icon-user"></i> Presenter</label>
+			<input type="text" class="input-xlarge" id="presenter" placeholder="Presenter">
+
+		
+      </form>
+	  
+	  </div>
+	  
+	  <div class="span8" style="margin-top:10px;">
+	  <table class="table table-bordered">
+	  <tr>
+		<td colspan="5" style="text-align:center;font-weight:bold;">***Rating***</td>
+	  </tr>
+<tr>
+      <td>1 - Good</td>
+      <td>2 - Good</td>
+	  <td>3 - Good</td>
+	  <td>4 - Good</td>
+	  <td>5 - Good</td>
+    </tr>
+	<tr>
+      <td>6 - Good</td>
+      <td>7 - Good</td>
+	  <td>8 - Good</td>
+	  <td>9 - Good</td>
+	  <td>10 - Good</td>
+    </tr>
+	
+</table>
+	  </div>
+		
 		
     </div>
 
