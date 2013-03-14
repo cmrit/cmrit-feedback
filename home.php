a77a6c1799a0743c4d63f2e671fef4bb829450f5
sachin200453@gmail.com <mailnarendra15@gmail.com>
2013-03-13 20:26:46 -0800

    added table of questions

--- home.php
+++ home.php
@@ -27,11 +27,7 @@
 
 		
             <label class="control-label" for="presenter"><i class="icon-user"></i> Presenter</label>
-			<input type="text" class="input-xlarge" id="presenter" placeholder="Presenter">
-
-		
-      </form>
-	  
+			<input type="text" class="input-xlarge" id="presenter" placeholder="Presenter">	  
 	  </div>
 	  
 	  <div class="span8" style="margin-top:10px;">
@@ -55,10 +51,197 @@
     </tr>
 	
 </table>
+	  </div>	
 	  </div>
-		
-		
-    </div>
+	  <div class="row-fluid">
+	  <div class="span12" style="margin-top:10px;">
+	  <table class="table table-bordered">
+	  <tr>
+		<td colspan="5" style="text-align:center;font-weight:bold;">***Rating***</td>
+	  </tr>
+<tr>
+	<td>1.</td>
+      <td>p is a pointer to a "constant integer". But we tried to change the value of the "constant integer".</td>
+      <td><select id="1">
+	   <option selected>-- Select --</option>
+ <option>1 - Good</option>
+ <option>2 - Good</option>
+ <option>3 - Good</option>
+ <option>4 - Good</option>
+ <option>5 - Good</option>
+ <option>6 - Good</option>
+ <option>7 - Good</option>
+ <option>8 - Good</option>
+ <option>9 - Good</option>
+ <option>10 - Good</option>
+</select></td>
+    </tr>
+
+	<tr>
+	<td>2.</td>
+      <td>p is a pointer to a "constant integer". But we tried to change the value of the "constant integer".</td>
+      <td><select id="2">
+	   <option selected>-- Select --</option>
+ <option>1 - Good</option>
+ <option>2 - Good</option>
+ <option>3 - Good</option>
+ <option>4 - Good</option>
+ <option>5 - Good</option>
+ <option>6 - Good</option>
+ <option>7 - Good</option>
+ <option>8 - Good</option>
+ <option>9 - Good</option>
+ <option>10 - Good</option>
+</select></td>
+    </tr>
+	
+	<tr>
+	<td>3.</td>
+      <td>p is a pointer to a "constant integer". But we tried to change the value of the "constant integer".</td>
+      <td><select id="3">
+	   <option selected>-- Select --</option>
+ <option>1 - Good</option>
+ <option>2 - Good</option>
+ <option>3 - Good</option>
+ <option>4 - Good</option>
+ <option>5 - Good</option>
+ <option>6 - Good</option>
+ <option>7 - Good</option>
+ <option>8 - Good</option>
+ <option>9 - Good</option>
+ <option>10 - Good</option>
+</select></td>
+    </tr>
+	
+	<tr>
+	<td>4.</td>
+      <td>p is a pointer to a "constant integer". But we tried to change the value of the "constant integer".</td>
+      <td><select id="4">
+	   <option selected>-- Select --</option>
+ <option>1 - Good</option>
+ <option>2 - Good</option>
+ <option>3 - Good</option>
+ <option>4 - Good</option>
+ <option>5 - Good</option>
+ <option>6 - Good</option>
+ <option>7 - Good</option>
+ <option>8 - Good</option>
+ <option>9 - Good</option>
+ <option>10 - Good</option>
+</select></td>
+    </tr>
+	
+	<tr>
+	<td>5.</td>
+      <td>p is a pointer to a "constant integer". But we tried to change the value of the "constant integer".</td>
+      <td><select id="5">
+	   <option selected>-- Select --</option>
+ <option>1 - Good</option>
+ <option>2 - Good</option>
+ <option>3 - Good</option>
+ <option>4 - Good</option>
+ <option>5 - Good</option>
+ <option>6 - Good</option>
+ <option>7 - Good</option>
+ <option>8 - Good</option>
+ <option>9 - Good</option>
+ <option>10 - Good</option>
+</select></td>
+    </tr>
+	
+	<tr>
+	<td>6.</td>
+      <td>p is a pointer to a "constant integer". But we tried to change the value of the "constant integer".</td>
+      <td><select id="6">
+	   <option selected>-- Select --</option>
+ <option>1 - Good</option>
+ <option>2 - Good</option>
+ <option>3 - Good</option>
+ <option>4 - Good</option>
+ <option>5 - Good</option>
+ <option>6 - Good</option>
+ <option>7 - Good</option>
+ <option>8 - Good</option>
+ <option>9 - Good</option>
+ <option>10 - Good</option>
+</select></td>
+    </tr>
+	
+	<tr>
+	<td>7.</td>
+      <td>p is a pointer to a "constant integer". But we tried to change the value of the "constant integer".</td>
+      <td><select id="7">
+	   <option selected>-- Select --</option>
+ <option>1 - Good</option>
+ <option>2 - Good</option>
+ <option>3 - Good</option>
+ <option>4 - Good</option>
+ <option>5 - Good</option>
+ <option>6 - Good</option>
+ <option>7 - Good</option>
+ <option>8 - Good</option>
+ <option>9 - Good</option>
+ <option>10 - Good</option>
+</select></td>
+    </tr>
+	
+	<tr>
+	<td>8.</td>
+      <td>p is a pointer to a "constant integer". But we tried to change the value of the "constant integer".</td>
+      <td><select id="8">
+	   <option selected>-- Select --</option>
+ <option>1 - Good</option>
+ <option>2 - Good</option>
+ <option>3 - Good</option>
+ <option>4 - Good</option>
+ <option>5 - Good</option>
+ <option>6 - Good</option>
+ <option>7 - Good</option>
+ <option>8 - Good</option>
+ <option>9 - Good</option>
+ <option>10 - Good</option>
+</select></td>
+    </tr>
+	
+	<tr>
+	<td>9.</td>
+      <td>p is a pointer to a "constant integer". But we tried to change the value of the "constant integer".</td>
+      <td><select id="9">
+	   <option selected>-- Select --</option>
+ <option>1 - Good</option>
+ <option>2 - Good</option>
+ <option>3 - Good</option>
+ <option>4 - Good</option>
+ <option>5 - Good</option>
+ <option>6 - Good</option>
+ <option>7 - Good</option>
+ <option>8 - Good</option>
+ <option>9 - Good</option>
+ <option>10 - Good</option>
+</select></td>
+    </tr>
+	
+	<tr>
+	<td>10.</td>
+      <td>p is a pointer to a "constant integer". But we tried to change the value of the "constant integer".</td>
+      <td><select id="10">
+	   <option selected>-- Select --</option>
+ <option>1 - Good</option>
+ <option>2 - Good</option>
+ <option>3 - Good</option>
+ <option>4 - Good</option>
+ <option>5 - Good</option>
+ <option>6 - Good</option>
+ <option>7 - Good</option>
+ <option>8 - Good</option>
+ <option>9 - Good</option>
+ <option>10 - Good</option>
+</select></td>
+    </tr>
+	
+</table>
+	  </div>	
+	  </div>
 
   </body>
 </html>
