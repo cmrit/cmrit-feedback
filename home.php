3b9f382656c654494ac6c21e505c043b4d40fc5f
sachin200453@gmail.com <mailnarendra15@gmail.com>
2013-03-13 19:14:03 -0800

    initialized home page

---
+++ home.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Login</title>
+    <link href="css/style.css" rel="stylesheet">
+	<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+	<script src="js/bootstrap.js"></script>
+  </head>
+
+  <body>
+		<div class="container">
+		
+    </div>
+
+  </body>
+</html>
