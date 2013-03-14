8b59589f10ac704b6f9f331a9659aa0ada0acb5c
sachin200453@gmail.com <mailnarendra15@gmail.com>
2013-03-13 19:13:16 -0800

    validated usn

--- index.php
+++ index.php
@@ -3,17 +3,39 @@
   <head>
     <title>Login</title>
     <link href="css/style.css" rel="stylesheet">
-
+	<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+	<script src="js/bootstrap.js"></script>
   </head>
 
   <body>
 		<div class="form-signin">
-      <form>
+		<div class="alert alert-error msg" id="invalid-alert">
+  <button type="button" class="close" data-dismiss="alert">&times;</button>
+  <strong>Invalid USN!!! Try Again!!</strong>
+</div>
+      <form id="f1">
         <h2 class="form-signin-heading">Enter your USN</h2>
-        <input type="text" class="input-block-level" placeholder="USN">
-        <button class="btn btn-large" type="submit">Log in</button>
+        <input type="text" class="input-block-level" placeholder="USN" id="usn" name="usn">
+        <input type="button" class="btn btn-large" id="usn-check" value="Log in">
       </form>
 
     </div>
+	<script>
+	$(document).ready(function()
+{
+$("#usn-check").click(function()
+{
+	var usn=$("#usn").val();
+	if(usn.match(/[1-4][a-zA-Z]{2}[0-9]{2}[a-zA-Z]{3}[0-9]{2}/))
+	{
+	var url="home.php?usn="+usn;
+	window.location=url;
+		}
+		else
+		$("#invalid-alert").removeClass("msg");
+});
+
+});
+	</script>
   </body>
 </html>
